c81924b19867a5b09cd4190caa01ed72dd369254
saiindirat <tholusaiindira@gmail.com>
2025-05-18 17:26:28 +0000

    Complete dice roll form with ordered list output

--- app.rb
+++ app.rb
@@ -2,8 +2,17 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get("/process_roll") do
+  @dice = params["dice"].to_i
+  @sides = params["sides"].to_i
+  @rolls = []
+
+  @dice.times do
+    @rolls.push(rand(1..@sides))
+  end
+
+  erb(:roll_results)
 end
